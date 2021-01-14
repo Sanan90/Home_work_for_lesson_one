6ae2220f96f8e3c7236c2daf27fb5455261dba1e
sanan90 <san-an@bk.ru>
2021-01-14 16:50:30 +0300

    Home work 3

---
+++ app/src/main/java/com/example/android/homeworkforlessonone/CalculatorLayout.java
@@ -0,0 +1,249 @@
+package com.example.android.homeworkforlessonone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CalculatorLayout extends AppCompatActivity implements View.OnClickListener {
+
+    private String number;
+    private String point = ".";
+    private TextView numbersText;
+    private Operations operations;
+    private final static String TAG = "[lifeActivity]";
+    private final static String keyOperations = "Operations";
+;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator_layout);
+
+        operations = new Operations();
+        initViews();
+    }
+
+
+
+    public void initViews() {
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button0 = findViewById(R.id.button0);
+        Button buttonPlus = findViewById(R.id.buttonPlus);
+        Button buttonMinus = findViewById(R.id.buttonMinus);
+        Button buttonTimes = findViewById(R.id.buttonTimes);
+        Button buttonDivided = findViewById(R.id.buttonDivided);
+        Button buttonEqual = findViewById(R.id.equally);
+        Button buttonPoint = findViewById(R.id.buttonPoint);
+        Button buttonC = findViewById(R.id.c);
+        Button buttonMplus = findViewById(R.id.mPlus);
+        Button buttonDelete = findViewById(R.id.delete);
+        numbersText = findViewById(R.id.forNumbers);
+
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        button0.setOnClickListener(this);
+        buttonPlus.setOnClickListener(this);
+        buttonMinus.setOnClickListener(this);
+        buttonTimes.setOnClickListener(this);
+        buttonDivided.setOnClickListener(this);
+        buttonDelete.setOnClickListener(this);
+        buttonPoint.setOnClickListener(this);
+        buttonC.setOnClickListener(this);
+        buttonEqual.setOnClickListener(this);
+        buttonMplus.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                number = "1";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.button2:
+                number = "2";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.button3:
+                number = "3";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.button4:
+                number = "4";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.button5:
+                number = "5";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.button6:
+                number = "6";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.button7:
+                number = "7";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.button8:
+                number = "8";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.button9:
+                number = "9";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.button0:
+                number = "0";
+                operations.display(number, numbersText.getText().toString());
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.buttonPlus:
+                operations.math("+");
+                numbersText.setText(operations.getOperationDisplay());
+                operations.setOperationDisplay(numbersText.getText().toString());
+                break;
+
+            case R.id.buttonMinus:
+                operations.math("-");
+                numbersText.setText(operations.getOperationDisplay());
+                operations.setOperationDisplay(numbersText.getText().toString());
+                break;
+
+            case R.id.buttonTimes:
+                operations.math("*");
+                numbersText.setText(operations.getOperationDisplay());
+                operations.setOperationDisplay(numbersText.getText().toString());
+                break;
+
+            case R.id.buttonDivided:
+                operations.math("/");
+                numbersText.setText(operations.getOperationDisplay());
+                operations.setOperationDisplay(numbersText.getText().toString());
+                break;
+
+            case R.id.equally:
+                operations.equal();
+                numbersText.setText(operations.getOperationDisplay());
+                operations.setOperationDisplay(numbersText.getText().toString());
+                break;
+
+            case R.id.delete:
+                operations.delete();
+                numbersText.setText(operations.getOperationDisplay());
+                operations.setOperationDisplay(numbersText.getText().toString());
+                break;
+
+            case R.id.buttonPoint:
+                operations.point(point);
+                numbersText.setText(operations.getOperationDisplay());
+                operations.setOperationDisplay(numbersText.getText().toString());
+                break;
+
+            case R.id.mPlus:
+                operations.mPlus();
+                numbersText.setText(operations.getOperationDisplay());
+                break;
+
+            case R.id.c:
+                operations.ac();
+                numbersText.setText(operations.getOperationDisplay());
+                operations.setOperationDisplay(numbersText.getText().toString());
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        operations = (Operations) savedInstanceState.getSerializable(keyOperations);
+        setOperations();
+    }
+
+    private void setOperations() {
+        operations.setOperationDisplay(operations.getOperationDisplay());
+        operations.setLastAtiphmeticSymbol(operations.getLastAtiphmeticSymbol());
+        operations.setLastButton(operations.getLastButton());
+        operations.setMaths(operations.getMaths());
+        operations.setmPlusMemory(operations.getmPlusMemory());
+        operations.setNum(operations.getNum());
+        operations.setNum2(operations.getNum2());
+        operations.setRestart(operations.getRestart());
+        operations.setStart(operations.getStart());
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(keyOperations, operations);
+    }
+}
---
+++ app/src/main/java/com/example/android/homeworkforlessonone/Operations.java
@@ -0,0 +1,333 @@
+package com.example.android.homeworkforlessonone;
+
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.text.DecimalFormat;
+
+class Operations implements Serializable {
+
+    private String operationDisplay;
+    private double mPlusMemory = 0;
+    private double num = 0;
+    private String maths = null;
+    private String point = ".";
+    private int restart = 0;
+    private String lastButton;
+    private String start = null;
+    private double num2 = -123456789;
+    private String lastAtiphmeticSymbol = null;
+
+    public Operations() {
+    }
+
+
+
+    public void setOperationDisplay(String operationDisplay) {
+        this.operationDisplay = operationDisplay;
+    }
+
+    public void setmPlusMemory(double mPlusMemory) {
+        this.mPlusMemory = mPlusMemory;
+    }
+
+    public void setNum(double num) {
+        this.num = num;
+    }
+
+    public void setMaths(String maths) {
+        this.maths = maths;
+    }
+
+    public void setPoint(String point) {
+        this.point = point;
+    }
+
+    public void setRestart(int restart) {
+        this.restart = restart;
+    }
+
+    public void setLastButton(String lastButton) {
+        this.lastButton = lastButton;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    public void setLastAtiphmeticSymbol(String lastAtiphmeticSymbol) {
+        this.lastAtiphmeticSymbol = lastAtiphmeticSymbol;
+    }
+
+    public String getOperationDisplay() {
+        return operationDisplay;
+    }
+
+    public double getmPlusMemory() {
+        return mPlusMemory;
+    }
+
+    public double getNum() {
+        return num;
+    }
+
+    public String getMaths() {
+        return maths;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+
+    public int getRestart() {
+        return restart;
+    }
+
+    public String getLastButton() {
+        return lastButton;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public String getLastAtiphmeticSymbol() {
+        return lastAtiphmeticSymbol;
+    }
+
+    public Operations(String operationDisplay) {
+        this.operationDisplay = operationDisplay;
+    }
+
+
+    public void display(String a, String b) {
+        lastButton = a;
+        if (restart != 1) {
+            if (b.equals("0") && a.equals("0")) {
+                return;
+            } else if (b.length() < 10) {
+                if (b.equals("0")) {
+                    operationDisplay = a;
+                } else {
+                    operationDisplay = b + a;
+                }
+            }
+        } else {
+            operationDisplay = a;
+            restart = 0;
+            return;
+        }
+    }
+
+
+    public void display(Double a) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        String forDisplay = decimalFormat.format(a);
+        operationDisplay = forDisplay;
+        rounding();
+    }
+
+
+    public void math(String math) {
+
+        if (lastButton == "+" || lastButton == "-" || lastButton == "*" || lastButton == "/") {
+            maths = math;
+            return;
+        }
+
+        if (start == null) {
+            num = (double) Double.valueOf((String) operationDisplay);
+            maths = math;
+            display((double) num);
+            restart = 1;
+            start = "2";
+            lastButton = math;
+            lastAtiphmeticSymbol = math;
+        } else if (start == "1") {
+            maths = math;
+            display((double) num);
+            restart = 1;
+            start = "2";
+            lastButton = math;
+            lastAtiphmeticSymbol = math;
+        } else {
+            if (maths.equals("-")) {
+                num -= (double) Double.valueOf((String) operationDisplay);
+                display((double) num);
+                maths = math;
+                restart = 1;
+                lastButton = math;
+                lastAtiphmeticSymbol = math;
+            } else if (maths.equals("+")) {
+                num += Double.valueOf((String) operationDisplay);
+                display((double) num);
+                maths = math;
+                restart = 1;
+                lastButton = math;
+                lastAtiphmeticSymbol = math;
+            } else if (maths.equals("*")) {
+                num *= Double.valueOf((String) operationDisplay);
+                display((double) num);
+                maths = math;
+                restart = 1;
+                lastButton = math;
+                lastAtiphmeticSymbol = math;
+            } else if (maths.equals("/")) {
+                num /= Double.valueOf((String) operationDisplay);
+                display((double) num);
+                maths = math;
+                restart = 1;
+                lastButton = math;
+                lastAtiphmeticSymbol = math;
+            }
+        }
+    }
+
+
+    public void equal() {
+
+        if (lastButton == "=") {
+            return;
+        }
+
+        lastButton = "=";
+        lastAtiphmeticSymbol = "=";
+        if (maths == null) {
+            return;
+
+        } else {
+            if (maths == "-") {
+                if (num2 == Double.valueOf((String) operationDisplay)) {
+                    num -= num2;
+                    num2 = -123456789;
+                    display((double) num);
+                    restart = 1;
+                    start = "1";
+                    maths = null;
+                } else {
+                    num -= Double.valueOf((String) operationDisplay);
+                    display((double) num);
+                    restart = 1;
+                    start = "1";
+                    maths = null;
+                }
+
+            } else if (maths == "+") {
+                if (num2 == Double.valueOf((String) operationDisplay)) {
+                    num += num2;
+                    num2 = -123456789;
+                    display((double) num);
+                    restart = 1;
+                    start = "1";
+                    maths = null;
+
+                } else {
+                    num += Double.valueOf((String) operationDisplay);
+                    display((double) num);
+                    restart = 1;
+                    start = "1";
+                    maths = null;
+                }
+
+            } else if (maths == "*") {
+                if (num2 == Double.valueOf((String) operationDisplay)) {
+                    num *= num2;
+                    num2 = -123456789;
+                    display((double) num);
+                    restart = 1;
+                    start = "1";
+                    maths = null;
+                } else {
+                    num *= Double.valueOf((String) operationDisplay);
+                    display((double) num);
+                    restart = 1;
+                    start = "1";
+                    maths = null;
+                }
+
+            } else if (maths == "/") {
+                if (num2 == Double.valueOf((String) operationDisplay)) {
+                    num /= num2;
+                    num2 = -123456789;
+                    display((double) num);
+                    restart = 1;
+                    start = "1";
+                    maths = null;
+                } else {
+                    num /= Double.valueOf((String) operationDisplay);
+                    display((double) num);
+                    restart = 1;
+                    start = "1";
+                    maths = null;
+                }
+            }
+        }
+    }
+
+    public void ac() {
+        num = 0;
+        maths = null;
+        restart = 0;
+        lastButton = null;
+        lastAtiphmeticSymbol = null;
+        start = null;
+        display((double) num);
+    }
+
+
+    public void point(String point) {
+        if (operationDisplay.length() > 0 && operationDisplay.length() < 10) {
+            for (int i = 0; i < operationDisplay.length(); i++) {
+                String word = String.valueOf(operationDisplay.charAt(i));
+                if (word.equals(point)) {
+                    return;
+                }
+            }
+            operationDisplay = operationDisplay + point;
+            lastButton = point;
+        }
+    }
+
+    public void delete() {
+        if (operationDisplay.length() < 1) {
+            return;
+        } else if (operationDisplay.length() == 1) {
+            operationDisplay = "0";
+            if (lastAtiphmeticSymbol == null || lastAtiphmeticSymbol == "=") {
+                num = Double.valueOf((String) operationDisplay);
+            } else {
+                num2 = Double.valueOf((String) operationDisplay);
+            }
+        } else {
+            String newText = "";
+            for (int i = 0; i < operationDisplay.length() - 1; i++) {
+                newText += String.valueOf(operationDisplay.charAt(i));
+            }
+            operationDisplay = newText;
+            if (lastAtiphmeticSymbol == null || lastAtiphmeticSymbol == "=") {
+                num = Double.valueOf((String) operationDisplay);
+            } else {
+                num2 = Double.valueOf((String) operationDisplay);
+            }
+        }
+    }
+
+    public void mPlus() {
+
+    }
+
+
+
+    public void rounding() {
+        double a = Double.parseDouble(operationDisplay);
+        num = a;
+    }
+}
